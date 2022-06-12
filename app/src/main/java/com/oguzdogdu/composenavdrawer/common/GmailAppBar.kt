371da1117006b7c89da6db8f17631f1eb69f514d
Oguzhan Dogdu <oguzd494@gmail.com>
2022-06-12 14:56:48 +0300

    Home app bar created

---
+++ app/src/main/java/com/oguzdogdu/composenavdrawer/common/GmailAppBar.kt
@@ -0,0 +1,71 @@
+package com.oguzdogdu.composenavdrawer.common
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+@Composable
+fun GmailAppBar(
+    scaffoldState: ScaffoldState,
+    scope: CoroutineScope
+) {
+    Box(
+        modifier = Modifier
+            .padding(10.dp)
+    ) {
+        Card(
+            modifier = Modifier.requiredHeight(50.dp),
+            shape = RoundedCornerShape(24.dp), elevation = 8.dp
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(8.dp)
+            ) {
+                IconButton(onClick = {
+                    scope.launch {
+                        scaffoldState.drawerState.open()
+                    }
+                }) {
+                    Icon(Icons.Default.Menu, "Menu")
+                }
+                Text(
+                    text = "Search in Mails",
+                    fontFamily = FontFamily.Default,
+                    modifier = Modifier.weight(2.0f)
+                )
+                Image(
+                    painter = painterResource(
+                        id = com.oguzdogdu.composenavdrawer.R
+                            .drawable.ic_launcher_background
+                    ),
+                    contentDescription = "Profile",
+                    modifier = Modifier
+                        .size(30.dp)
+                        .clip(CircleShape)
+                        .background(color = Color.Gray)
+                        .clickable {
+                        }
+
+                )
+            }
+        }
+    }
+}
