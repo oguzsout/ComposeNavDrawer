58ebccdfd2dcda867632a54852cf3f16cc4fb1d1
Oguzhan Dogdu <oguzd494@gmail.com>
2022-06-26 14:54:55 +0300

    finished project

--- app/src/main/java/com/oguzdogdu/composenavdrawer/MainActivity.kt
+++ app/src/main/java/com/oguzdogdu/composenavdrawer/MainActivity.kt
@@ -3,13 +3,18 @@ package com.oguzdogdu.composenavdrawer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.tooling.preview.Preview
+import com.oguzdogdu.composenavdrawer.common.GmailAppBar
+import com.oguzdogdu.composenavdrawer.common.GmailDrawerMenu
 import com.oguzdogdu.composenavdrawer.ui.theme.ComposeNavDrawerTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +22,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeNavDrawerTheme {
-                // A surface container using the 'background' color from the theme
+                window?.statusBarColor = Color.White.toArgb()
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    GmailApp()
                 }
             }
         }
@@ -30,14 +34,25 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun GmailApp() {
+    val scaffoldState = rememberScaffoldState()
+    val coroutineScope = rememberCoroutineScope()
+
+    Scaffold(
+        scaffoldState = scaffoldState,
+        topBar = { GmailAppBar(scaffoldState, coroutineScope) },
+        drawerContent = {
+            GmailDrawerMenu()
+        }
+    ) {
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeNavDrawerTheme {
-        Greeting("Android")
+       GmailApp()
     }
 }
